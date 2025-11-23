9e3fde17167bed5a81a069bd9ac1bfe70b9fae2e
Temaaaaaaaaaaa <gundorinartem@gmail.com>
2025-11-23 13:51:14 +0300

    a

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package LethalCompany
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+	log.Print("Серва на http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+	}
+}
